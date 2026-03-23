728693b5eff8dbbd81f4bffbf1df6651772f1986
Kuber Sethi <ksethi@palantir.com>
2026-03-23 14:06:40 -0400

    replace hand-rolled svgs with blueprintjs icons

--- packages/cbac-components/src/cbac-picker/base/BaseCbacBannerPopover.tsx
+++ packages/cbac-components/src/cbac-picker/base/BaseCbacBannerPopover.tsx
@@ -15,6 +15,7 @@
  */
 
 import { Popover } from "@base-ui/react/popover";
+import { CaretDown, CaretUp, WarningSign } from "@blueprintjs/icons";
 import { ActionButton } from "@osdk/react-components/primitives";
 import classnames from "classnames";
 import React from "react";
@@ -99,7 +100,9 @@ export function BaseCbacBannerPopover({
                 open && styles.caretIconVisible,
               )}
             >
-              {open ? <CaretUpIcon /> : <CaretDownIcon />}
+              {open
+                ? <CaretUp size={12} color="currentColor" />
+                : <CaretDown size={12} color="currentColor" />}
             </span>
           </div>
         }
@@ -228,7 +231,7 @@ const PopoverContent = React.memo(function PopoverContent({
             <p className={styles.warningsHeader}>Warnings and Notices</p>
             {warnings.map((warning, i) => (
               <div key={`warning-${i}`} className={styles.warningCallout}>
-                <WarningIcon />
+                <WarningSign className={styles.warningIcon} size={14} />
                 <p className={styles.warningText}>{warning}</p>
               </div>
             ))}
@@ -248,46 +251,3 @@ const PopoverContent = React.memo(function PopoverContent({
     </>
   );
 });
-
-function CaretDownIcon(): React.ReactElement {
-  return (
-    <svg
-      width="12"
-      height="12"
-      viewBox="0 0 12 12"
-      fill="none"
-      aria-hidden="true"
-    >
-      <path d="M3 4.5L6 7.5L9 4.5" stroke="currentColor" strokeWidth="1.5" />
-    </svg>
-  );
-}
-
-function CaretUpIcon(): React.ReactElement {
-  return (
-    <svg
-      width="12"
-      height="12"
-      viewBox="0 0 12 12"
-      fill="none"
-      aria-hidden="true"
-    >
-      <path d="M3 7.5L6 4.5L9 7.5" stroke="currentColor" strokeWidth="1.5" />
-    </svg>
-  );
-}
-
-function WarningIcon(): React.ReactElement {
-  return (
-    <svg
-      className={styles.warningIcon}
-      width="14"
-      height="14"
-      viewBox="0 0 14 14"
-      fill="currentColor"
-      aria-hidden="true"
-    >
-      <path d="M7 1L13 13H1L7 1ZM7 3.5L2.5 12H11.5L7 3.5ZM6.5 7V9H7.5V7H6.5ZM6.5 10V11H7.5V10H6.5Z" />
-    </svg>
-  );
-}
