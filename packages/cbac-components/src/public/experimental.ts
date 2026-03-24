c7c49a9f72f4b8596fcfc6983d5ed30bb026bd6c
Kuber Sethi <ksethi@palantir.com>
2026-03-24 14:41:37 -0400

    remove duplicate base component exports, fix dependency sort order

--- packages/cbac-components/src/public/experimental.ts
+++ packages/cbac-components/src/public/experimental.ts
@@ -14,11 +14,6 @@
  * limitations under the License.
  */
 
-// CBAC Picker - Base components
-export { BaseCbacBanner } from "../cbac-picker/base/BaseCbacBanner.js";
-export { BaseCbacPicker } from "../cbac-picker/base/BaseCbacPicker.js";
-export { BaseCbacPickerDialog } from "../cbac-picker/base/BaseCbacPickerDialog.js";
-
 // CBAC Picker - Selection logic utilities
 export {
   computeMarkingStates,
