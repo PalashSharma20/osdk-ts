49a2e6110eb99bc84b1bcc3040ca9330f51203ee
Kuber Sethi <ksethi@palantir.com>
2026-03-23 20:24:54 -0400

    add shape transformations, link resolution, and observable fixes

--- packages/client/src/observable/internal/list/ListQuery.ts
+++ packages/client/src/observable/internal/list/ListQuery.ts
@@ -56,6 +56,7 @@ import {
   ORDER_BY_IDX,
   PIVOT_IDX,
   RDP_IDX,
+  RIDS_IDX,
   SELECT_IDX,
   WHERE_IDX,
 } from "./ListCacheKey.js";
@@ -170,6 +171,10 @@ export abstract class ListQuery extends BaseListQuery<
 
   get canonicalPivotInfo(): Canonical<PivotInfo> | undefined {
     return this.#pivotInfo;
+  }
+
+  get canonicalRids(): Canonical<string[]> | undefined {
+    return this.cacheKey.otherKeys[RIDS_IDX];
   }
 
   protected abstract createObjectSet(
@@ -343,7 +348,9 @@ export abstract class ListQuery extends BaseListQuery<
       );
     }
 
-    if (changes.modified.has(this.cacheKey)) return;
+    if (changes.modified.has(this.cacheKey)) {
+      return;
+    }
     // mark ourselves as updated so we don't infinite recurse.
     changes.modified.add(this.cacheKey);
 
@@ -409,7 +416,9 @@ export abstract class ListQuery extends BaseListQuery<
         }
 
         for (const key of existingList) {
-          if (toRemove.has(key)) continue;
+          if (toRemove.has(key)) {
+            continue;
+          }
           newList.push(key);
         }
         for (const obj of toAdd) {
